ed4722fbdad2dec52c07d259e600955a02c1c3cf
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2023-12-11 17:33:03 +0100

    job a finir

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import pygame
-surf = pygame.display.set_mode((800,600))
-def draw_board():
-    run = True
-    while run :
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-            run = False
-      pygame.draw.line(surf,(255,255,255),(250,0),(250,600),2)
-      pygame.draw.line(surf,(255,255,255),(540,0),(540,600),2)
-      pygame.draw.line(surf,(255,255,255),(0,195),(800,195),2)
-      pygame.draw.line(surf,(255,255,255),(0,405),(800,405),2)
-      pygame.display.flip()
-draw_board()
 
-def draw_cross():
-   run = True
-   while run:
-      for event in pygame.event.get():
+surf = pygame.display.set_mode((800,600))
+run = True
+
+def draw_board():
+   pygame.draw.line(surf,(255,255,255),(250,0),(250,600),2)
+   pygame.draw.line(surf,(255,255,255),(540,0),(540,600),2)
+   pygame.draw.line(surf,(255,255,255),(0,195),(800,195),2)
+   pygame.draw.line(surf,(255,255,255),(0,405),(800,405),2)
+   pygame.display.flip()
+
+def draw_cross(pos2):
+      pygame.draw.line(surf,(255,255,255),(pos2),(pos2),5)
+      pygame.draw.line(surf,(255,255,255),(pos2),(pos2),5)
+      pygame.display.flip()
+
+def draw_circle(pos):
+   pygame.draw.circle (surf ,(255,255,255),(pos),80 , 5)
+   pygame.display.flip()
+
+while run:
+     for event in pygame.event.get():
          if event.type == pygame.QUIT:
             run = False
-      pygame.draw.line(surf,(255,255,255),(10,20),(150,200),2)
-      pygame.draw.line(surf,(255,255,255),(150,10),(20,200),2)
-      pygame.display.flip()
-   pygame.quit()
+         elif event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed() == (1,0,0) :
+               draw_circle(pygame.mouse.get_pos())
+         elif event.type == pygame.MOUSEBUTTONDOWN:
+             if pygame.mouse.get_pressed() == (0,0,1) :
+                 draw_cross(pygame.mouse.get_pos())
+     draw_board()
 
-draw_cross()
-
-   
-		
+pygame.quit()
--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import pygame
 
 surf = pygame.display.set_mode((800,600))
-run = True
-posX = 50
-vx = 1
-img = pygame.image.load("TPN.png")
+run =  True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-          run = False
-    surf.fill((0,0,0))
-    surf.blit(img ,(-550,-200))
-    pygame.draw.line(surf,(255,255,255),(10,20),(150,200),2)
-    pygame.draw.line(surf,(255,255,255),(150,10),(20,200),2)
-    pygame.draw.circle(surf,(255,0,0),(400,100), 80,2)
-    
-    pygame.display.flip()
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed() == (1,0,0) :
+                pos = pygame.mouse.get_pos()
+            pygame.draw.circle (surf ,(255,255,255),(pos),80 , 5)
+            pygame.display.flip()
 
-pygame.quit()
+    pygame.quit
